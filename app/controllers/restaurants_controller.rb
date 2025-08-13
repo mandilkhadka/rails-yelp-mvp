3713d8780738293778656f4af311b4ea2881f9eb
Mandil Khadka <khadkamandil62@gmail.com>
2025-08-13 14:32:24 +0900

    work

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,26 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def create
+    @restaurant = Restaurant.new(strong_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def strong_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
